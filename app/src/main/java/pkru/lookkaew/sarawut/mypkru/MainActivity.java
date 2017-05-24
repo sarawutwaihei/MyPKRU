0223a79d87b6f205b7cdb3fe0b9670fb99d232b7
sarawut <sarawut5711423129@gmail.com>
2017-05-24 12:11:11 +0700

    Finish 23may

--- app/src/main/java/pkru/lookkaew/sarawut/mypkru/MainActivity.java
+++ app/src/main/java/pkru/lookkaew/sarawut/mypkru/MainActivity.java
@@ -1,13 +1,63 @@
 package pkru.lookkaew.sarawut.mypkru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit การประกาศตัวแปล ประกอบด้วย 3 อย่าง 1. Access 2. Type ชนิดของตัวแปล 3. Name
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initial View การเชื่อมต่อใน .xml
+        initialView();
+
+        //Controller
+        Controller();
+
+
+    }  // Main Method
+
+    private void Controller() {
+        textView.setOnClickListener(this);
+        button.setOnClickListener(this);
+
     }
-}
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //For TextView
+        if (v == textView) {
+            //Intent NewRegister
+            Intent intent = new Intent(MainActivity.this, NewRegisterActivity.class);
+            startActivity(intent);
+        }
+
+        //For button
+        if (v == button) {
+        }
+
+
+    }
+}   // Main class คลาสหลัก
