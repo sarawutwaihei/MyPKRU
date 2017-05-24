bb2e169483eaac2bcc458f62bdc9008bce8cfac0
sarawut <sarawut5711423129@gmail.com>
2017-05-24 15:59:14 +0700

    human Ok

--- app/src/main/java/pkru/lookkaew/sarawut/mypkru/NewRegisterActivity.java
+++ app/src/main/java/pkru/lookkaew/sarawut/mypkru/NewRegisterActivity.java
@@ -1,13 +1,96 @@
 package pkru.lookkaew.sarawut.mypkru;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class NewRegisterActivity extends AppCompatActivity {
+public class NewRegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private ImageView backImageView, humanImageView, cameraImageView;
+    private Button button;
+    private Uri humanUri, camaraUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_register2);
+
+        //Initial View
+        initialView();
+
+        //Controller
+        controller();
+
+    } //  Main Method
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //For Human
+        if ((requestCode == 0) && (resultCode == RESULT_OK)) {
+            Log.d("24MayV1", "Human OK");
+
+            //Show Image
+            humanUri = data.getData();
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(humanUri));
+                humanImageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                Log.d("24May", "e humanUri ==> " + e.toString());
+            }
+
+
+        }
     }
-}
+
+    private void controller() {
+
+        backImageView.setOnClickListener(this);
+        humanImageView.setOnClickListener(this);
+        cameraImageView.setOnClickListener(this);
+        button.setOnClickListener(this);
+
+    }
+
+    private void initialView() {
+
+        nameEditText = (EditText) findViewById(R.id.edtName);
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        backImageView = (ImageView) findViewById(R.id.btnBack);
+        humanImageView = (ImageView) findViewById(R.id.imvHumen);
+        cameraImageView = (ImageView) findViewById(R.id.imvCamera);
+        button = (Button) findViewById(R.id.btnRegister);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //For Back
+        if (v == backImageView) {
+            finish();
+        }
+
+        //For Human
+        if (v == humanImageView) {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivityForResult(Intent.createChooser(intent, "Please Choose App for Choose Image"), 0);
+        }
+
+    }
+} // Main Class
